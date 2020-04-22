c4851ff76893a0c50d13c54deb30256efd8386d3
Hassia T <hassia.tamimou@gmail.com>
2020-04-22 17:18:06 +0200

    adding elementary sorting algorithms file

---
+++ C++/SortingLibrary/elementarysorts.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<vector>
+template<typename U>
+bool compareU(U i, U j);
+
+
+template<typename U>
+void selectionSort(std::vector<U>&);
+
+template<typename U>
+void insertionSort(std::vector<U>&);
+
+template<typename U>
+void shellSort(std::vector<U>&);
