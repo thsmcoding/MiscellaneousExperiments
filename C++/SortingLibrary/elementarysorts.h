201456f5314c4d2f5b25eb691bdf2d1009f0228d
htscoding <hassia.tamimou@gmail.com>
2020-04-25 23:17:46 +0200

    insertion improved + bubble insertion

--- C++/SortingLibrary/elementarysorts.h
+++ C++/SortingLibrary/elementarysorts.h
@@ -2,17 +2,15 @@
 #include<vector>
 
 template<typename U> inline bool compareU(U i, U j);
-
-
 template<typename U> inline void selectionSort(std::vector<U>&);
-
-
+template<typename U> inline void insertionSort(std::vector<U>&);
+template<typename U> inline void insertionSortimproved(std::vector<U>&);
+template<typename U> inline void bubbleSort(std::vector<U> &);
 template<typename U>  bool compareU(U i, U j) {
 	return i < j;
 }
 
-
-
+/****** FUCNTION DEFINITIONS ************************************************/
 template<typename U> void selectionSort(std::vector<U>& vec1) {
 	for (int i = 0; i < vec1.size(); i++) {
 		auto current = vec1[i];
@@ -33,9 +31,45 @@ template<typename U> void selectionSort(std::vector<U>& vec1) {
 	}
 }
 
-//template<typename U>
-//void insertionSort(std::vector<U>&);
+template<typename U> void insertionSort(std::vector<U>& vec) {
+	for (int i = 1; i < vec.size(); i++) {
+		for (int j = i; j > 0 && (vec[j] < vec[j - 1]); --j) {
+			auto item_j = vec[j];
+			auto item_j_1 = vec[j - 1];
+			vec[j] = item_j_1;
+			vec[j - 1] = item_j;
+		}
+	}
+}
 
-//template<typename U>
-//void shellSort(std::vector<U>&);
+template<typename U> void insertionSortimproved(std::vector<U>& vec) {
+	for (int i = 1; i < vec.size(); i++) {
+		auto current = vec[i];
+		int j = i;
+		for (j = i; j > 0 && (current < vec[j - 1]); --j) {
+			auto item_j_1 = vec[j - 1];
+			vec[j] = item_j_1;
+		}
+		vec[j] = current;
+	}
+}
 
+
+template<typename U> void bubbleSort(std::vector<U> &vec) {
+	auto N = vec.size();
+	bool swapped = false;
+	for (int i = 0; i < N; i++) {
+		swapped = (swapped == true) ? false : swapped;
+		for (int j = 0; j < N - i - 1; j++) {
+			if (vec[j] > vec[j + 1]) {
+				auto current = vec[j];
+				auto current_next = vec[j + 1];
+				vec[j] = current_next;
+				vec[j + 1] = current;
+				swapped = true;
+			}
+		}
+		if (swapped == false)
+			break;
+	}
+}
--- C++/SortingLibraryTests/test.cpp
+++ C++/SortingLibraryTests/test.cpp
@@ -1,9 +1,9 @@
 #include "pch.h"
 #include <vector>
-
 #include "../SortingLibrary/elementarysorts.h"
 #include <algorithm>
 #include <string>
+#include "../gtest/gtest.h"
 
 using namespace std;
 
@@ -19,7 +19,6 @@ TEST(INTEGERVECTORCASE, selectionSortFunction) {
 	std::vector<int> vector1 = { -1,0,7,8,-1,2,7,100,-400,35,38 };
 	std::vector<int> vector1_bis = { -1,0,7,8,-1,2,7,100,-400,35,38 };
 	std::sort(vector1.begin(), vector1.end());
-
 	selectionSort<int>(vector1_bis);
 	EXPECT_EQ(vector1, vector1_bis);
 }
@@ -37,6 +36,30 @@ TEST(STRINGCASE, selectionSortFunction) {
 	std::vector<std::string> vector1_bis = { "maman", "happy", "friday","here" };
 	std::sort(vector1.begin(), vector1.end());
 	selectionSort<std::string>(vector1_bis);
+	EXPECT_EQ(vector1, vector1_bis);
+}
+
+TEST(INTEGERINSERTION, insertionSortFunction) {
+	std::vector<int> vector1 = { 100,-1,0,-15,-17,207,85,14,63,789,157,41,-45 };
+	std::vector<int> vector1_bis = { 100,-1,0,-15,-17,207,85,14,63,789,157,41,-45 };
+	std::sort(vector1.begin(), vector1.end());
+	insertionSort<int>(vector1_bis);
+	EXPECT_EQ(vector1, vector1_bis);
+}
+
+TEST(INTEGERINSERTIONIMRPOVED, insertionSortimprovedFunction) {
+	std::vector<int> vector1 = { 100,-1,0,-15,-17,207,85,14,63,789,157,41,-45 };
+	std::vector<int> vector1_bis = { 100,-1,0,-15,-17,207,85,14,63,789,157,41,-45 };
+	std::sort(vector1.begin(), vector1.end());
+	insertionSortimproved<int>(vector1_bis);
+	EXPECT_EQ(vector1, vector1_bis);
+}
+
+TEST(INTEGERBUBBLESORT, bubbleSortFunction) {
+	std::vector<int> vector1 = {0,100,-1,0,-15,-17,207,85,14,63,789,157,41,-45 };
+	std::vector<int> vector1_bis = { 0,100,-1,0,-15,-17,207,85,14,63,789,157,41,-45 };
+	std::sort(vector1.begin(), vector1.end());
+	bubbleSort<int>(vector1_bis);
 	EXPECT_EQ(vector1, vector1_bis);
 }
 
